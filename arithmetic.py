9fdf926a38dbc89c4508f801f83514fcd6667d2c
Hackbright Student <info@hackbrightacademy.com>
2014-10-01 13:03:39 -0700

    working calcul.code

---
+++ arithmetic.py
@@ -0,0 +1,25 @@
+def add(x , y):
+    return x + y
+def subtract(x, y):
+    return x - y
+def multiply(x, y):
+    return x * y
+
+def divide(divisor, dividend):
+    # this only returns the quotient not the remainder
+    return divisor / dividend  
+def square(base):
+    return base ** 2
+def cube(base):
+    return square(base) * base
+def power(base, exponent):
+    pass
+    # for num in range [1:exponent]
+            #base ** exponent
+
+            # if expo 1
+               #return base
+            #if expo even
+                #return...
+            #if expo odd
+                #return...
