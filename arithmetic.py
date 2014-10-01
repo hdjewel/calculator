5500aa3ddab38bbdb51d995f76bf185e29e64b21
Hackbright Student <info@hackbrightacademy.com>
2014-10-01 16:29:50 -0700

    first save of arith.

--- arithmetic.py
+++ arithmetic.py
@@ -8,13 +8,32 @@ def multiply(x, y):
 def divide(divisor, dividend):
     # this only returns the quotient not the remainder
     return divisor / dividend  
-def square(base):
-    return base ** 2
-def cube(base):
+
     return square(base) * base
+
+def cube(x):
+    return x ** 3
+
 def power(base, exponent):
-    pass
-    # for num in range [1:exponent]
+    
+    return_value = 1
+    quotient = exponent / 3
+
+    if quotient == 0:
+        remainder = exponent % 3
+
+        if remainder == 0:
+            return_value = 1
+        elif remainder == 1:
+            return_value = base
+        else:
+            return_value = base * base
+    else:
+        for num in range (0, quotient):
+            return_value = return_value * cube(base)
+
+    return return_value
+
             #base ** exponent
 
             # if expo 1
@@ -23,3 +42,8 @@ def power(base, exponent):
                 #return...
             #if expo odd
                 #return...
+def main():
+    print power(2,7)
+
+
+main()
